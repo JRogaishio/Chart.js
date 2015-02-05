fe26c1826ced7c48a0d683cffdc207488862a38d
jrogaishio <jrogaishio@mindedge.com>
2015-02-05 13:59:24 -0500

    Setup multi-line x-axis labels

--- Chart.js
+++ Chart.js
@@ -8,6 +8,33 @@
  * https://github.com/nnnick/Chart.js/blob/master/LICENSE.md
  */
 
+//Prototype for handling multiline fillText
+CanvasRenderingContext2D.prototype.wrapText = function (text, x, y, maxWidth, lineHeight) {
+
+    var lines = text.split("\n");
+
+    for (var i = 0; i < lines.length; i++) {
+
+        var words = lines[i].split(' ');
+        var line = '';
+
+        for (var n = 0; n < words.length; n++) {
+            var testLine = line + words[n] + ' ';
+            var metrics = this.measureText(testLine);
+            var testWidth = metrics.width;
+            if (testWidth > maxWidth && n > 0) {
+                this.fillText(line, x, y);
+                line = words[n] + ' ';
+                y += lineHeight;
+            } else {
+                line = testLine;
+            }
+        }
+
+        this.fillText(line, x, y);
+        y += lineHeight;
+    }
+};
 
 (function(){
 
@@ -1712,7 +1739,7 @@
 					ctx.font = this.font;
 					ctx.textAlign = (isRotated) ? "right" : "center";
 					ctx.textBaseline = (isRotated) ? "middle" : "top";
-					ctx.fillText(label, 0, 0);
+					ctx.wrapText(label,0,0, 100,16)
 					ctx.restore();
 				},this);
 
